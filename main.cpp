1f44fee47c6e5a541dc14b09742dd60321a57975
cezary95 <41197984+cezary95@users.noreply.github.com>
2018-07-13 18:03:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@ using namespace std;
 
 int main() {
 
-
-
-
-
+	package klasy
+	public class Klasy {
+		public static void main (String [] args) {
+			class Komputer{
+				String nazwa;
+				int wiek;
+				double cena
+		void wyswietl_wartosci_pol(){
+		system.out.println ("nazwa: "+ nazwa);
+		system.out.println ("wiek: "+ wiek);
+		system.out.println ("cena "+ cena);
+			
+		
 	cin.ignore();
 	getchar();
 	return 0;
